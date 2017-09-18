7264b884bc48a7b98e1330e24be6687df1756f75
Lucas Vieira <lima.lucaslv@gmail.com>
2017-09-18 00:19:25 -0300

    Initial structure

---
+++ src/eventPage.js
@@ -0,0 +1,21 @@
+var menuItem = {
+	"id": "cambridge_dict",
+	"title": "CambridgeDict",
+	"contexts": ["selection"]
+};
+
+chrome.contextMenus.create(menuItem);
+
+function encodeURI (str) {
+	var replaced = str.toLowerCase().replace(/ /g, '-');
+    return replaced;
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData) {
+	if (clickData.menuItemId == "cambridge_dict" && clickData.selectionText) {
+		var cambridgeDictUrl = "https://dictionary.cambridge.org/us/dictionary/english/" 
+			+ encodeURI(clickData.selectionText);
+
+		window.open(cambridgeDictUrl, "dictWindow");
+	}
+});
